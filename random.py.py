741ca11cf3f6bc8cb1cb0aa7671d822bbe1e3af0
lonnie20 <lonbregman@gmail.com>
2015-11-18 17:04:20 +0100

    Random werkt!!1!!!

--- random.py.py
+++ random.py.py
@@ -1,5 +1,5 @@
-import visual
 import random
+import visual
 
 #Sizes Board
 widthBoard = 17
@@ -7,19 +7,20 @@ heightBoard = 17
 sizeTile = 20
 
 grid = [[0]*widthBoard for n in range(heightBoard)]
-tiles = sorted([2,3,3,3,3,3,3,3,5,5,5,7,7,7], reverse=True)
+tiles = [2,3,3,3,3,3,3,3,5,5,5,7,7,7]
 
+placedTilesDict = {2:[],3:[],5:[],7:[]}
 placedTiles = []
-placedCounter = 0
 colorTile = 1
 
-def tilePlacer(tile, colorTile, randomTileNumber):
-#Finding a zero in the grid where the tile wouldn't go outside the board.
+def tilePlacer(tile, colorTile):
     groundZero = False
+    
+#Finding a zero in the grid where the tile wouldn't go outside the board.
+    y = 0
     for row in grid:
-        y = grid.index(row)
+        x = 0
         for gridValue in row:
-            x = row.index(gridValue)
             if gridValue == 0 and x+tile <= widthBoard and y+tile <= heightBoard:
                 groundZero = True
                 
@@ -29,34 +30,49 @@ def tilePlacer(tile, colorTile, randomTileNumber):
                         if grid[i][j] > 0:
                             groundZero = False
                             break
-                    if groundZero == False:
+                    if not groundZero:
                         break
                 else:
                     for i in range(y, y+tile):
                         for j in range(x, x+tile):
                             grid[i][j] += colorTile
-                    return [x,y]
+                    #return [x,y]
+                    return True
                 
+            x += 1
+        y += 1
+        
     if not groundZero:
         return False
+    
+countercounter = 0
+trials = 100
+for i in range(trials):
+    counter = 0
+    tiles = [2,3,3,3,3,3,3,3,5,5,5,7,7,7]
+    while tiles != []:
+        counter += 1
+        grid = [[0]*widthBoard for n in range(heightBoard)]
+        tiles = [2,3,3,3,3,3,3,3,5,5,5,7,7,7]
 
+        for i in range(100):
+            if len(tiles)>0:
+                randomTileNumber = random.randint(0,len(tiles)-1)
+                tile = tiles[randomTileNumber]
+                #coordPlacedTile = tilePlacer(tile, colorTile)
+                #if coordPlacedTile:
+                if tilePlacer(tile, colorTile):
+                    #placedTilesDict[tile].append(coordPlacedTile)
+                    #placedTiles.append(tile)
+                    tiles.remove(tiles[randomTileNumber])
+                    colorTile += 1
+                    #x = placedTilesDict.get(placedTiles[-1])[-1][0]
+            else:
+                break
+    countercounter += counter
 
-henkie = {2:[],3:[],5:[],7:[]}
-for i in range(1000):
-    randomTileNumber = random.randint(0,len(tiles)-1)
-    tile = tiles[randomTileNumber]
-    coordPlacedTile = tilePlacer(tile, colorTile, randomTileNumber)
-    if coordPlacedTile:
-        henkie[tile].append(coordPlacedTile)
-        colorTile += 1
-        placedTiles.append(tiles[randomTileNumber])
-        placedCounter += 1
-        tiles.remove(tiles[randomTileNumber])
-        print henkie
-        print tiles
-    #if not coordPlacedTile:
-     #   print placedTiles[-1]
-            
+    print counter
+print countercounter/trials
 #Open visualization.
 while(True):
     visualization = visual.visualizationGrid(widthBoard, heightBoard, sizeTile, grid)
