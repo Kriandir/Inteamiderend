3fe74541457e93f473a5a5d12a6fa568620b29c2
lonnie20 <lonbregman@gmail.com>
2015-11-30 16:16:12 +0100

    yolo

--- visual.py
+++ visual.py
@@ -21,22 +21,37 @@ class visualizationGrid:
         self.blue =(0,0,255)
         self.pink = (255,0,255)
         self.yellow = (255,255,0)
+<<<<<<< HEAD
+        self.orange = (200,100,0)
+        self.purple = (150,0,255)
+        
+        self.colors = [self.white, self.red, self.blue, self.pink, self.yellow,self.orange, self.purple]
+        self.numberOfColors = len(self.colors)
+        
+=======
         self.orange = (255,100,0)
         self.purple = (150,0,255)
         self.colorlist = [self.black, self.white, self.red, self.blue, self.pink, self.yellow,self.orange, self.purple]
+>>>>>>> 7c90535aaf090f64a6b2cd7db0b67aa3767210f2
     def drawGrid(self):
         y = 0
         for row in self.grid:
             x = 0
             for col in row:
+<<<<<<< HEAD
+                col = col-int(col/self.numberOfColors)*self.numberOfColors
+                for i in range(self.numberOfColors):
+                    if col == i:
+                        pygame.draw.rect(self.screen, self.colors[i],(x, y, self.sizeTile, self.sizeTile))
+=======
                 for i in range(1,len(self.colorlist)):
                     if col % i == 0:
                         pygame.draw.rect(self.screen,self.colorlist[i],(x,y, self.sizeTile,self.sizeTile))
                 if col == 0:
                     pygame.draw.rect(self.screen, self.black, (x, y, self.sizeTile, self.sizeTile))
+>>>>>>> 7c90535aaf090f64a6b2cd7db0b67aa3767210f2
                 pygame.draw.rect(self.screen, self.green, (x, y, self.sizeTile, self.sizeTile), 1)
                 x += self.sizeTile
-
             y += self.sizeTile
 
         pygame.display.update()
