2e89006802ce771197ec3efb40bbaf13153e6c32
lonnie20 <lonbregman@gmail.com>
2015-12-09 00:14:02 +0100

    yolo

--- recursieveShizzleRect.py
+++ recursieveShizzleRect.py
@@ -2,7 +2,7 @@ import visual
 import copy
 import itertools
 
-def solveBoard(board,vis):
+def solveBoard(board,showVisual):
     if board == 1:
         widthBoard = 17
         heightBoard = 17
@@ -17,61 +17,57 @@ def solveBoard(board,vis):
         widthBoard = 55
         heightBoard = 56
         tiles = [[20,21],[19,20],[19,18],[18,17],[16,17],[16,15],[14,15],[14,13],[12,13],[12,11],[10,11],[9,10],[9,8],[7,8],[7,6],[6,5],[5,4],[4,3],[2,3],[1,2]]
-        sizeTile = 8
-
+        sizeTile = 10
+    elif board == 4: # Eerste bord VU.
+        widthBoard = 12
+        heightBoard = 17
+        tiles = [[5,5],[6,4],[6,4],[5,4],[5,3],[5,3],[7,2],[6,2],[6,2],[5,2],[4,2],[3,2],[5,1],[4,1],[3,1],[2,1]]
+        sizeTile = 20
+                
     emptyGrid = [[0]*widthBoard for n in range(heightBoard)]
     colorTile = 0
 
-#Places the tile on the first possible position in the grid. Else returns False.
+#If it fits, places the tile on the given position in the grid. Else returns False.
     def tilePlacer(grid,x,y,tileX,tileY,colorTile):
-        groundZero = True
         if x+tileX <= widthBoard and y+tileY <= heightBoard:
             for i in range(y, y+tileY):
                 for j in range(x, x+tileX):
                     if grid[i][j] > 0:
-                        groundZero = False
-                        break
-                if not groundZero:
-                    break
-            else:
-                for i in range(y, y+tileY):
-                    for j in range(x, x+tileX):
+                        return False
+                    else:
                         grid[i][j] = colorTile
-                return grid
+            return grid
 
-        return False
-
-#Receives a grid and a list with tiles which are not placed yet, and returns a list with all possible children (where one more tile is placed).
+#Receives a grid and a list of tiles, and returns a list with all possible children (where one more tile is placed).
     def generateAllChildren(parent,tiles,colorTile):
         children = []
         colorTile += 1
-        copyTiles1 = copy.deepcopy(tiles)
-        copyTiles1 = list(copyTiles1 for copyTiles1,_ in itertools.groupby(copyTiles1))
+        uniqueTiles = list(tiles for tiles,_ in itertools.groupby(tiles)) #To avoid duplicate children.
         y = 0
         for row in parent:
             x = 0
             for gridValue in row:
                 if gridValue == 0:
-                    for tile in copyTiles1:
+                    for tile in uniqueTiles:
                         copyParent = copy.deepcopy(parent)
                         tileX = tile[0]
                         tileY = tile[1]
                         gridWithPlacedTile = tilePlacer(copyParent,x,y,tileX,tileY,colorTile)
                         if gridWithPlacedTile:
-                            copyTiles2 = copy.deepcopy(tiles)
-                            copyTiles2.remove(tile)
-                            children.append([gridWithPlacedTile,copyTiles2,colorTile])
+                            copyTiles = list(tiles)
+                            copyTiles.remove(tile)
+                            children.append([gridWithPlacedTile,copyTiles,colorTile])
                     return children
 
                 x += 1
             y += 1
 
-
+#Recursive function for depth-first search for the solution of the board.
     def searchForSolution(parent,tiles,colorTile):
         if tiles:
             children = generateAllChildren(parent,tiles,colorTile)
             for child in children:
-                if vis:
+                if showVisual:
                     visual.visualizationGrid(widthBoard,heightBoard,sizeTile,child[0]).drawGrid()
                 solution = searchForSolution(child[0],child[1],child[2])
                 if solution:
@@ -83,4 +79,4 @@ def solveBoard(board,vis):
     solution = searchForSolution(emptyGrid,tiles,colorTile)
     visual.visualizationGrid(widthBoard,heightBoard,sizeTile,solution).drawGrid()
     
-solveBoard(1,False)
+solveBoard(1,True)
